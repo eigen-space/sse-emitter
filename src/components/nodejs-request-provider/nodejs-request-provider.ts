4817c6ee4d5a7619a65772a615fd84440eaa9481
Nikita Agupov <nktrsk@gmail.com>
2020-11-23 17:22:12 +0300

    #1 common: moved http error to base client, added build to pre push hook

--- src/components/nodejs-request-provider/nodejs-request-provider.ts
+++ src/components/nodejs-request-provider/nodejs-request-provider.ts
@@ -1,8 +1,7 @@
-import { RequestProviderResponse } from '@eigenspace/base-http-client';
+import { HttpError, RequestProviderResponse } from '@eigenspace/base-http-client';
 import { RequestProvider, RequestProviderOptions } from '@eigenspace/base-http-client/types/request-provider';
 import fetch, { Response } from 'node-fetch';
 import { NodejsProviderResponse } from '../nodejs-provider-response/nodejs-provider-response';
-import { HttpError } from '../../entities/http-error/http-error';
 
 export class NodejsRequestProvider implements RequestProvider<Response> {
 
