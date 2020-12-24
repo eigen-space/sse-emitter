8b34126fe7de65497fc202943ae511181ccce463
Danilqa <hello.boriskas@gmail.com>
2020-12-24 23:29:29 +0300

    #1 app: added pipeline for middlewares

--- src/app/sse-emitter.ts
+++ src/app/sse-emitter.ts
@@ -4,6 +4,8 @@ import { AnyDictionary } from '@eigenspace/common-types';
 import { Logger } from '@eigenspace/logger';
 import { HttpStatusCode } from '../enums/http-status-code.enum';
 import { IdUtils } from '../utils/id/id.utils';
+import { Middleware } from '../types/middleware';
+import { Pipeline } from '../entities/pipeline';
 
 export class SseEmitter {
     private static ON_DATA_RECEIVED_EVENT = 'ON_DATA_RECEIVED';
@@ -11,39 +13,53 @@ export class SseEmitter {
     private eventEmitter = new EventEmitter();
     private logger = new Logger({ component: 'SseEmitter' });
 
-    constructor() {
-        this.init = this.init.bind(this);
+    init(...middlewares: Middleware[]): (req: Request, res: Response) => void {
+        return (req: Request, res: Response) => {
+            this.logger.info('init', 'ready');
+
+            // Otherwise the connection will be closed
+            req.socket.setTimeout(0);
+
+            // When a TCP connection is created, it will have Nagle's algorithm enabled.
+            // Nagle's algorithm delays data before it is sent via the network.
+            // It attempts to optimize throughput at the expense of latency.
+            req.socket.setNoDelay(true);
+
+            // Use one HTTP-connection for multiple transactions
+            req.socket.setKeepAlive(true);
+
+            res.status(HttpStatusCode.OK);
+
+            res.setHeader('Content-Type', 'text/event-stream');
+            res.setHeader('Cache-Control', 'no-cache');
+
+            // Remove Nginx buffering
+            res.setHeader('X-Accel-Buffering', 'no');
+            if (req.httpVersion !== '2.0') {
+                res.setHeader('Connection', 'keep-alive');
+            }
+
+            // Increase number of event listeners on init
+            this.eventEmitter.setMaxListeners(Infinity);
+
+            const onDataReceived = (data: AnyDictionary): void => {
+                const pipeline = new Pipeline(...middlewares, this.sendData(data));
+                pipeline.execute(req, res);
+            };
+
+            this.eventEmitter.on(SseEmitter.ON_DATA_RECEIVED_EVENT, onDataReceived);
+
+            req.on('close', () => this.eventEmitter.removeListener('data', onDataReceived));
+        };
     }
 
-    init(req: Request, res: Response): void {
-        this.logger.info('init', 'ready');
+    send(data: AnyDictionary): void {
+        this.logger.info('send', `${JSON.stringify(data)}`);
+        this.eventEmitter.emit(SseEmitter.ON_DATA_RECEIVED_EVENT, { data });
+    }
 
-        // Otherwise the connection will be closed
-        req.socket.setTimeout(0);
-
-        // When a TCP connection is created, it will have Nagle's algorithm enabled.
-        // Nagle's algorithm delays data before it is sent via the network.
-        // It attempts to optimize throughput at the expense of latency.
-        req.socket.setNoDelay(true);
-
-        // Use one HTTP-connection for multiple transactions
-        req.socket.setKeepAlive(true);
-
-        res.status(HttpStatusCode.OK);
-
-        res.setHeader('Content-Type', 'text/event-stream');
-        res.setHeader('Cache-Control', 'no-cache');
-
-        // Remove Nginx buffering
-        res.setHeader('X-Accel-Buffering', 'no');
-        if (req.httpVersion !== '2.0') {
-            res.setHeader('Connection', 'keep-alive');
-        }
-
-        // Increase number of event listeners on init
-        this.eventEmitter.setMaxListeners(Infinity);
-
-        const onDataReceived = (data: AnyDictionary): void => {
+    private sendData(data: AnyDictionary): Middleware {
+        return (_: Request, res: Response, next: () => void) => {
             this.logger.info('send', 'sending data...');
             const id = IdUtils.generateNumber();
 
@@ -52,15 +68,8 @@ export class SseEmitter {
             res.write(`id: ${id}\n`);
             // \n\n - is necessary part to any SSE messages
             res.write(`data: ${JSON.stringify(data.data)}\n\n`);
+
+            next();
         };
-
-        this.eventEmitter.on(SseEmitter.ON_DATA_RECEIVED_EVENT, onDataReceived);
-
-        req.on('close', () => this.eventEmitter.removeListener('data', onDataReceived));
-    }
-
-    send(data: AnyDictionary): void {
-        this.logger.info('send', `${JSON.stringify(data)}`);
-        this.eventEmitter.emit(SseEmitter.ON_DATA_RECEIVED_EVENT, { data });
     }
 }
